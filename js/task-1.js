d374337a35e0733d02ac7c67a37285f7f736353b
Oleksandr Dmytriev <sanderdsukr@gmail.com>
2025-06-13 13:04:08 +0300

    init

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const userName = "John";
+const userLastName = "Doe";
+const fullName = `${userName} ${userLastName}`
+// let userInput = prompt('Input your age:')
+// const age = Number(userInput);
+
+// console.log(fullName);
+// console.log(userInput);
